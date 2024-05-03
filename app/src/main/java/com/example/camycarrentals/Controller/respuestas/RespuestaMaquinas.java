79376b79b4021047724db34ddac7a69c612d7b82
MatoRV <victor.mato@metadata.es>
2024-05-03 10:09:24 +0200

    Borrado codigo no utilizado

--- app/src/main/java/com/example/camycarrentals/Controller/respuestas/RespuestaMaquinas.java
+++ app/src/main/java/com/example/camycarrentals/Controller/respuestas/RespuestaMaquinas.java
@@ -1,17 +1,10 @@
 package com.example.camycarrentals.Controller.respuestas;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 import com.example.camycarrentals.Model.Maquina;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 public class RespuestaMaquinas {
 
@@ -42,16 +35,6 @@ public class RespuestaMaquinas {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
-//
-//        JsonElement jsonElement = JsonParser.parseString(this.datos);
-//
-//        JsonObject json = jsonElement.getAsJsonObject().getAsJsonObject();
-//        JsonArray jsonLista = json.getAsJsonArray();
-//
-//        for (int i = 0; i < jsonLista.size(); i++) {
-//            dataList.add(new Maquina(jsonLista.get(i).getAsJsonArray().get(0).getAsJsonPrimitive().getAsInt(),jsonLista.get(i).getAsJsonArray().get(1).getAsJsonPrimitive().getAsString(),jsonLista.get(i).getAsJsonArray().get(2).getAsJsonPrimitive().getAsString(),
-//                    jsonLista.get(i).getAsJsonArray().get(3).getAsJsonPrimitive().getAsInt(),jsonLista.get(i).getAsJsonArray().get(4).getAsJsonPrimitive().getAsString(),jsonLista.get(i).getAsJsonArray().get(5).getAsJsonPrimitive().getAsString()));
-//        }
 
         return dataList;
     }
